7fe0bb32c73d9a1261e9293b1fed076db1535e83
juncent crispino <juncent.crispino@gmail.com>
2021-10-27 21:04:20 +0800

    About Me Component: unordered list

---
+++ src/components/AboutMe.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function AboutMe(){
+	return(
+		<div className="text-center mt-5">
+			About Me
+			<ul>
+				<li>Full-Stack Web Dev</li>
+				<li>Civil Engineer</li>
+				<li>Freelancer</li>
+			</ul>
+		</div>
+	)
+}
