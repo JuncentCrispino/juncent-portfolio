744d850f2e2bcc1ad0126d7a32a48a5671728532
juncent crispino <juncent.crispino@gmail.com>
2021-10-28 21:50:38 +0800

    updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ export default function Footer(){
 	return(
 		<footer style={footerStyle}>
             <div className="container">
-                <span className="text-muted">Place sticky footer content here.</span>
+                <span className="text-muted">This is the footer</span>
             </div>
         </footer>
 	)
